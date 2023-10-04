36d1f033e14aefa01e9aae019657ad651a63a88a
Lucas Nobre <lucasnobreoficial@hotmail.com>
2023-10-04 13:56:54 -0300

    Progresso - Introdução a Estruturas de repetição

---
+++ introducao-estrutura-de-repeticao-em-c#/forandwhile/forandwhile/Program.cs
@@ -0,0 +1,26 @@
+﻿for (int i = 0; i < 11; ++i )
+{
+    Console.WriteLine( $"{2} * {i} = {2*i}" );
+}
+
+int j = 0;
+
+Console.WriteLine("=================");
+
+while (j < 11)
+{
+    Console.WriteLine($"{3} * {j} = {3 * j}");
+    j++;
+
+    if (j == 10) break;
+}
+
+Console.WriteLine("=================");
+
+int x = 0;
+
+do
+{
+    Console.WriteLine($"{4} * {x} = {4 * x}");
+    x++;
+} while (x < 11);
